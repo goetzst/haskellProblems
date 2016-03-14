6599c423a454c06956f35ad817e163ec49104bbe
Stefan Goetz <goetzst@hotmail.de>
2016-03-14 23:55:52 +0100

    problem 2

--- app/Main.hs
+++ app/Main.hs
@@ -22,9 +22,24 @@ p1'' xs = (head . drop (length xs -1)) xs
 
 p1''' xs  = xs !! (length xs -1)
 
+-- taken from https://wiki.haskell.org/99_questions/1_to_10
 p1'''' :: [a] -> a
 p1'''' = foldr1 (curry snd)
 {----------- PROBLEM 2 -----------}
 -- Find the last but one element of a list.
 -- without Maybe
 -- as safe as head
+p2 :: [a] -> a
+p2 (x:[y])  = x
+p2 (x:xs)   = p2 xs
+
+p2' :: [a] -> a
+p2' xs = head $ drop (length xs -2) xs
+
+p2'' xs = xs !! (length xs -2)
+
+p2''' (_:xs)  = p1 xs
+
+-- taken from https://wiki.haskell.org/99_questions/1_to_10
+p2'''' :: [a] -> Maybe a 
+p2'''' = fst . foldl (\(a,b) c -> (b, Just c)) (Nothing, Nothing)
